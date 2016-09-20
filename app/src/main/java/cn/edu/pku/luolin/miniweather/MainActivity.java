46dfccffeadbe5f1ec0216b187bef2d140533948
luolin <luolin@pku.edu.cn>
2016-09-20 19:36:36 +0800

    init project

---
+++ app/src/main/java/cn/edu/pku/luolin/miniweather/MainActivity.java
@@ -0,0 +1,16 @@
+package cn.edu.pku.luolin.miniweather;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by luolin on 2016/9/20.
+ */
+public class MainActivity extends Activity{
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.weather_info);
+    }
+}
