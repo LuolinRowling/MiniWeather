07fd5da0b4117006acfe793a4d018c0fe0700f14
luolin <luolin@pku.edu.cn>
2016-10-11 21:24:22 +0800

    -获取网络数据 -更新今日天气数据（文字与图片） -制作选择城市界面（ing，新建了activity）

---
+++ app/src/main/java/cn/edu/pku/luolin/miniweather/SelectCity.java
@@ -0,0 +1,16 @@
+package cn.edu.pku.luolin.miniweather;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by luolin on 2016/10/11.
+ */
+public class SelectCity extends Activity{
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.select_city);
+    }
+}
